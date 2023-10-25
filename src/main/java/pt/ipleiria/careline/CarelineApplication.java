310690d2362898080c2d2cf425c13679d7d8162e
Ivo Afonso Bispo <ivoafobispo@gmail.com>
2023-10-25 18:56:16 +0100

    base spring initializr

---
+++ src/main/java/pt/ipleiria/careline/CarelineApplication.java
@@ -0,0 +1,13 @@
+package pt.ipleiria.careline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarelineApplication.class, args);
+	}
+
+}
