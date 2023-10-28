f39848f7b4cb8a35f15af95ba13fffc47d0eea42
Yunikyon <anaritamsmartins@gmail.com>
2023-10-28 18:30:51 +0100

    conflict resolver

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,45 +1,13 @@
-<<<<<<< HEAD
-import logo from './logo.svg';
-import './App.css';
-=======
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
 import Header from './Components/Header/Header';
 import ClientLeftColumn from './Components/ClientComponents/LeftColumn';
 import ClientRightColumn from './Components/ClientComponents/RightColumn';
 import VerticalBar from './VerticalBar';
->>>>>>> 687fd8c4c2eb82b30f6d338123edc32273c1c29f
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-      <div className='App-side-bar'>
-        <div>A</div>
-        <div>B</div>
-        <div>C</div>
-      </div>
-
-      <div className='App-body'>
-        <div className='App-header'>
-          <div className='App-header-name'>Welcome back, Helena</div>
-          <div className='App-header-logo'>Image</div>
-        </div>  
-        <div className='App-content'>
-          <div className='App-client-colum'>
-            <div className='App-client-human-layout'>Human Layout</div>
-            <div className='App-client-measure-buttons'>
-              <div className='App-client-measure-button'>A</div>
-              <div className='App-client-measure-button'>B</div>
-            </div>
-          </div>
-          <div className='App-divider-bar'></div>
-          <div className='App-client-column'>
-            <div className='App-client-datalist'></div>
-            <hr></hr>
-            <div className='App-client-datalist'></div>
-          </div>
-=======
       <Navbar />
 
       <div className='App-body'>
@@ -48,7 +16,6 @@ function App() {
           <ClientLeftColumn />
           <VerticalBar />
           <ClientRightColumn />
->>>>>>> 687fd8c4c2eb82b30f6d338123edc32273c1c29f
         </div>
       </div>
     </div>
