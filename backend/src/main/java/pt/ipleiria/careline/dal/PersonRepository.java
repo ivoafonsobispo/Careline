65f13739ef9aa5c50edf24d9de1e3c487a5b4e45
Ivo Afonso Bispo <ivoafobispo@gmail.com>
2023-11-02 17:20:31 +0000

    Deleted Java Spring Boot Backend

--- backend/src/main/java/pt/ipleiria/careline/dal/PersonRepository.java
+++
@@ -1,7 +0,0 @@
-package pt.ipleiria.careline.dal;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import pt.ipleiria.careline.entities.Person;
-
-public interface PersonRepository extends JpaRepository<Person, Integer> {
-}
