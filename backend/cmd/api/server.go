87531833daf094fd66902478e28e48afb953b54a
Ivo Afonso Bispo <ivoafobispo@gmail.com>
2023-11-09 19:27:00 +0000

    Chapter 11 - Graceful Shutdown

--- backend/cmd/api/server.go
+++ backend/cmd/api/server.go
@@ -1,14 +1,18 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"fmt"
 	"log/slog"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 )
 
 func (app *application) serve() error {
-	// Declare a HTTP server using the same settings as in our main() function.
 	srv := &http.Server{
 		Addr:         fmt.Sprintf(":%d", app.config.port),
 		Handler:      app.routes(),
@@ -18,9 +22,34 @@ func (app *application) serve() error {
 		ErrorLog:     slog.NewLogLogger(app.logger.Handler(), slog.LevelError),
 	}
 
-	// Likewise log a "starting server" message.
+	shutdownError := make(chan error)
+
+	go func() {
+		quit := make(chan os.Signal, 1)
+		signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+		s := <-quit
+
+		app.logger.Info("caught signal", "signal", s.String())
+
+		ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+		defer cancel()
+
+		shutdownError <- srv.Shutdown(ctx)
+	}()
+
 	app.logger.Info("starting server", "addr", srv.Addr, "env", app.config.env)
 
-	// Start the server as normal, returning any error.
-	return srv.ListenAndServe()
+	err := srv.ListenAndServe()
+	if !errors.Is(err, http.ErrServerClosed) {
+		return err
+	}
+
+	err = <-shutdownError
+	if err != nil {
+		return nil
+	}
+
+	app.logger.Info("stopped server", "addr", srv.Addr)
+
+	return nil
 }
