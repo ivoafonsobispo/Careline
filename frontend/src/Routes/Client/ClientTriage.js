8bf0b562b5e34b4c36c9684ce23d31b612ca28ef
Yunikyon <anaritamsmartins@gmail.com>
2023-12-22 15:38:52 +0000

    Front-end client triage layout

--- frontend/src/Routes/Client/ClientTriage.js
+++ frontend/src/Routes/Client/ClientTriage.js
@@ -1,12 +1,42 @@
 import PageTitle from "../../Components/PageTitle/PageTitle";
 import "../../Components/ClientComponents/ClientBase.css";
+import ClientTriageComponent from "../../Components/ClientComponents/ClientTriageComponent";
+
+import {useState} from 'react';
+
+//Day Picker
+import { format } from 'date-fns';
+import { DayPicker } from 'react-day-picker';
+import 'react-day-picker/dist/style.css';
+import '../../DayPicker.css';
 
 export default function ClientTriage() {
+    const [selected, setSelected] = useState(new Date());
+
+    let footer = <p>Please pick a day.</p>;
+    if (selected) {
+        footer = <p>You picked {format(selected, 'PP')}.</p>;
+    }
+
     return(
-        <div className="vertical-container">
-            <PageTitle title="Triage"/>
-            <div className='App-content'>
-                
+        <div className="horizontal-container">
+            <div className="vertical-container">
+                <PageTitle title="Triage"/>
+                <div className='App-content'>
+                    <div className="vertical-container diagnoses-list" style={{maxHeight: "540px"}}>
+                        <ClientTriageComponent status={"Reviewed"}/>
+                        <ClientTriageComponent status={"Sent"}/>
+                    </div>
+                </div>
+            </div>
+            <div className='day-picker'>
+                <DayPicker
+                    mode="single"
+                    selected={selected}
+                    onSelect={setSelected}
+                    footer={footer}
+                    className='day-picker-style'
+                />
             </div>
         </div>
     );
