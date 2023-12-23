7d32e5eff949379ffd8a19ee5458a810200870ed
Yunikyon <anaritamsmartins@gmail.com>
2023-12-23 23:19:56 +0000

    front-end web sockets attempt

--- frontend/src/Routes/Client/ClientMeasures.js
+++ frontend/src/Routes/Client/ClientMeasures.js
@@ -8,12 +8,24 @@ import { DayPicker } from 'react-day-picker';
 import 'react-day-picker/dist/style.css';
 import '../../DayPicker.css';
 
+import useWebSocket from 'react-use-websocket';
+
 import axios from 'axios';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 const urlHeartbeats = 'http://localhost:8080/api/patients/1/heartbeats/latest';
 const urlTemperatures = 'http://localhost:8080/api/patients/1/temperatures/latest';
 
+
 export default function ClientMeasures() {
+  const socketUrl = 'ws://localhost:8080/app/websocket-endpoint/topic/heartbeats';
+
+  const { lastMessage, readyState, onMessage } = useWebSocket(socketUrl, {
+    onMessage: (event) => {
+      console.log('Received message:', event.data);
+      // You can perform additional actions with the received message here
+    },
+  });
+
 
   const [selected, setSelected] = useState(new Date());
 
@@ -21,6 +33,14 @@ export default function ClientMeasures() {
   if (selected) {
     footer = <p>You picked {format(selected, 'PP')}.</p>;
   }
+
+  // const handleMessage = (event) => {
+  //   console.log('Received message:', event.data);
+
+  // };
+
+  // // Set up the event listener
+  // onMessage(handleMessage);
 
   const [heartbeats, setHeartbeats] = useState(null);
   const [temperatures, setTemperatures] = useState(null);
@@ -57,6 +77,22 @@ export default function ClientMeasures() {
       .catch(error => {
         console.log(error);
       });
+
+    // const socket = new WebSocket("ws://localhost:8080");
+
+    // // Connection opened
+    // socket.addEventListener("open", (event) => {
+    //   socket.send("Connection established")
+    // })
+
+    // // Listen for messages
+    // socket.addEventListener("message", (event) => {
+    //   console.log("Message from server ", event.data)
+    // })
+
+    // connection.current = ws
+
+    // return () => connection.close()
   }, []);
 
   if (!heartbeats) return null;
