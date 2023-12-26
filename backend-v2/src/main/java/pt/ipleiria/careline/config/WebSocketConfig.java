c5cdb43c0758f7ac5fc72fd0df44ecde4f20adff
Yunikyon <anaritamsmartins@gmail.com>
2023-12-26 11:33:43 +0000

    backend websocketconfig crossorigin

--- backend-v2/src/main/java/pt/ipleiria/careline/config/WebSocketConfig.java
+++ backend-v2/src/main/java/pt/ipleiria/careline/config/WebSocketConfig.java
@@ -1,12 +1,15 @@
 package pt.ipleiria.careline.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
+
 @Configuration
 @EnableWebSocketMessageBroker
+@CrossOrigin
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
@@ -17,6 +20,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/websocket-endpoint").withSockJS();
+        registry.addEndpoint("/websocket-endpoint").setAllowedOrigins("http://localhost:3000").withSockJS();
     }
 }
