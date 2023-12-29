ad4a9e979b0e6033b40a7a8872ac4cce10282c11
Ivo Afonso Bispo <ivoafobispo@gmail.com>
2023-12-29 11:49:47 +0000

    Diagnosis PDF

--- backend-v2/src/main/java/pt/ipleiria/careline/utils/PdfGenerator.java
+++ backend-v2/src/main/java/pt/ipleiria/careline/utils/PdfGenerator.java
@@ -1,13 +1,45 @@
 package pt.ipleiria.careline.utils;
 
 import com.lowagie.text.*;
+import com.lowagie.text.pdf.PdfPTable;
 import com.lowagie.text.pdf.PdfWriter;
 import pt.ipleiria.careline.domain.entities.DiagnosisEntity;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class PdfGenerator {
+    private static PdfPTable drawMedicationTable(DiagnosisEntity diagnosisEntity) throws IOException {
+        PdfPTable medications = new PdfPTable(5);
+        medications.addCell("Name");
+        medications.addCell("Dosage");
+        medications.addCell("Frequency");
+        medications.addCell("Duration");
+        medications.addCell("QR Code");
+
+        for (int i = 0; i < diagnosisEntity.getMedications().size(); i++) {
+            medications.addCell(diagnosisEntity.getMedications().get(i).getName());
+            medications.addCell(diagnosisEntity.getMedications().get(i).getDosage());
+            medications.addCell(diagnosisEntity.getMedications().get(i).getFrequency());
+            medications.addCell(diagnosisEntity.getMedications().get(i).getDuration());
+
+            String medicationURL = "https://www.kuantokusta.pt/search?q=";
+            medicationURL += diagnosisEntity.getMedications().get(i).getName();
+            byte[] qrCodeImage = null;
+            try {
+                qrCodeImage = QRCodeGenerator.getQRCodeImage(medicationURL, 100, 100);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            Image qrCode = Image.getInstance(qrCodeImage);
+            medications.addCell(qrCode);
+        }
+        return medications;
+    }
+
     public byte[] generateDiagnosisPDF(DiagnosisEntity diagnosisEntity) throws DocumentException, IOException {
         // Create document with A4 size
         Document document = new Document(PageSize.A4);
@@ -27,35 +59,30 @@ public class PdfGenerator {
             fontTitle.setSize(16);
 
             // Title
-            Paragraph title = new Paragraph("Diagnosis: " + diagnosisEntity.getCreatedAt(), fontTitle);
+            ZonedDateTime createdAt = ZonedDateTime.ofInstant(diagnosisEntity.getCreatedAt(), ZoneId.systemDefault());
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("EEEE, MMM dd 'AT' HH:mm");
+            String formattedDate = createdAt.format(formatter);
+            Paragraph title = new Paragraph("Diagnosis from: " + formattedDate, fontTitle);
             title.setAlignment(Paragraph.ALIGN_CENTER);
             title.setSpacingAfter(20);
 
             // Patient information
             Paragraph patient = new Paragraph("Patient: " + diagnosisEntity.getPatient().getName()
                     + " | Email: " + diagnosisEntity.getPatient().getEmail()
-                    + " | Numero Utente de Saude: " + diagnosisEntity.getPatient().getNus());
+                    + " | NUS: " + diagnosisEntity.getPatient().getNus());
 
             // Professional information
             Paragraph professional = new Paragraph("Professional: " + diagnosisEntity.getProfessional().getName()
                     + " | Email: " + diagnosisEntity.getProfessional().getEmail()
-                    + " | Numero Utente de Saude: " + diagnosisEntity.getProfessional().getNus());
+                    + " | NUS: " + diagnosisEntity.getProfessional().getNus());
 
-            // Diagnosis and Prescriptions
+            // Diagnosis
             Paragraph diagnosis = new Paragraph("Diagnosis: " + diagnosisEntity.getDiagnosis());
-            Paragraph prescriptions = new Paragraph("Prescriptions: " + diagnosisEntity.getMedications());
 
-            // QR Code
-            String medication = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
-            byte[] qrCodeImage = null;
-            try {
-                qrCodeImage = QRCodeGenerator.getQRCodeImage(medication, 100, 100);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-
-            Image qrCode = Image.getInstance(qrCodeImage);
-            qrCode.setAlignment(Paragraph.ALIGN_BOTTOM);
+            // Generate Medications Table
+            Paragraph medicationsParagraph = new Paragraph("Medications:");
+            medicationsParagraph.setSpacingAfter(20);
+            PdfPTable medications = drawMedicationTable(diagnosisEntity);
 
             // Add elements to the document
             document.addCreationDate();
@@ -65,8 +92,8 @@ public class PdfGenerator {
             document.add(patient);
             document.add(professional);
             document.add(diagnosis);
-            document.add(prescriptions);
-            document.add(qrCode);
+            document.add(medicationsParagraph);
+            document.add(medications);
 
             // Close the document
             document.close();
@@ -75,5 +102,6 @@ public class PdfGenerator {
         // Return the generated PDF as a byte array
         return byteArrayOutputStream.toByteArray();
     }
+
 
 }
