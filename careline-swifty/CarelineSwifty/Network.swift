a496f9b907d0fc6171860361b7efe6bfdbba17dc
ivoafonsobispo <ivoafobispo@gmail.com>
2024-01-05 19:48:09 +0000

    Triage POST

--- careline-swifty/CarelineSwifty/Network.swift
+++ careline-swifty/CarelineSwifty/Network.swift
@@ -243,6 +243,33 @@ struct HeartbeatItem: Codable {
     }
 }
 
+struct TemperatureContent: Codable {
+    let content: [TemperatureItem]
+    let pageable: Pageable
+    let last: Bool
+    let totalElements: Int
+    let totalPages: Int
+    let size: Int
+    let number: Int
+    let sort: Sort
+    let first: Bool
+    let numberOfElements: Int
+    let empty: Bool
+}
+
+
+struct TemperatureItem: Codable {
+    let severity: String
+    let temperature: Double
+    let createdAt: String
+    
+    enum CodingKeys: String, CodingKey {
+        case severity
+        case temperature
+        case createdAt = "created_at"
+    }
+}
+
 struct Pageable: Codable {
     let pageNumber: Int
     let pageSize: Int
@@ -312,3 +339,107 @@ class APIHeartbeatGET: ObservableObject {
     }
 }
 
+class APITemperatureGETCareline: ObservableObject {
+    var bearerToken: String = ""
+    var latestTemperatureSeverity: String?
+    var latestTemperatureValue: Double?
+    var latestTemperatureCreatedAt: String?
+    
+    func makeTemperatureGetRequest(completion: @escaping (Result<Void, Error>) -> ()) {
+        guard let url = URL(string: "http://10.20.229.55/api/patients/1/temperatures/latest?size=1") else {
+            print("Invalid URL")
+            return
+        }
+
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        
+        // Set the authorization header with the bearer token
+        request.addValue("Bearer \(bearerToken)", forHTTPHeaderField: "Authorization")
+
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            if let error = error {
+                completion(.failure(error))
+                return
+            }
+
+            guard let httpResponse = response as? HTTPURLResponse,
+                  (200...299).contains(httpResponse.statusCode) else {
+                let statusCode = (response as? HTTPURLResponse)?.statusCode ?? -1
+                completion(.failure(NSError(domain: "HTTP Error", code: statusCode, userInfo: nil)))
+                return
+            }
+
+            do {
+                if let data = data {
+                    let decoder = JSONDecoder()
+                    let temperatureContent = try decoder.decode(TemperatureContent.self, from: data)
+                    if let firstTemperature = temperatureContent.content.first {
+                        self.latestTemperatureSeverity = firstTemperature.severity
+                        self.latestTemperatureValue = firstTemperature.temperature
+                        self.latestTemperatureCreatedAt = firstTemperature.createdAt
+                        completion(.success(()))
+                        print("Latest temperature Severity: \(self.latestTemperatureSeverity)")
+                        print("Latest temperature Value: \(self.latestTemperatureValue)")
+                        print("Latest temperature Created At: \(self.latestTemperatureCreatedAt)")
+                    }
+                }
+            } catch {
+                completion(.failure(error))
+            }
+        }
+        
+        task.resume()
+    }
+}
+
+class APITriagePOST: ObservableObject {
+    var bearerToken: String = ""
+    
+    func makeTriagePostRequest(symptoms: String,temperature: Double,heartbeat: Int, completion: @escaping (Error?) -> Void) {
+        guard let url = URL(string: "http://10.20.229.55/api/patients/1/triages") else {
+            print("Invalid URL")
+            return
+        }
+        
+        let requestData: [String: Any] = [
+            "heartbeat": String(heartbeat),
+            "temperature": String(temperature),
+            "symptoms": symptoms
+        ]
+        
+        do {
+            // Convert the requestData to JSON data
+            let jsonData = try JSONSerialization.data(withJSONObject: requestData)
+
+            // Create the URLRequest with the specified URL and method
+            var request = URLRequest(url: url)
+            request.httpMethod = "POST"
+
+            // Set the request body with the JSON data
+            request.httpBody = jsonData
+
+            // Set the content type to JSON
+            request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+            
+            // Set the authorization header with the bearer token
+            request.addValue("Bearer \(bearerToken)", forHTTPHeaderField: "Authorization")
+
+            // Create a URLSession task for the request
+            URLSession.shared.dataTask(with: request) { data, response, error in
+                if let error = error {
+                    print("Error: \(error)")
+                } else if let response = response as? HTTPURLResponse {
+                    if(response.statusCode == 201) {
+                        print("POST request successful")
+                    } else {
+                        print("POST request failed with status code: \(response.statusCode)")
+                    }
+                }
+            }.resume()
+        } catch {
+            print("Error converting requestData to JSON data: \(error)")
+        }
+        
+    }
+}
