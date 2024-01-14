379fd91b76bd784a44f2c320ac0201546d4abc20
Ivo Afonso Bispo <ivoafobispo@gmail.com>
2024-01-14 10:12:01 +0000

    Logs

--- backend-v2/src/main/java/pt/ipleiria/careline/config/RateLimitInterceptor.java
+++ backend-v2/src/main/java/pt/ipleiria/careline/config/RateLimitInterceptor.java
@@ -4,6 +4,7 @@ import io.github.bucket4j.Bucket;
 import io.github.bucket4j.ConsumptionProbe;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -12,6 +13,7 @@ import org.springframework.web.servlet.HandlerInterceptor;
 import pt.ipleiria.careline.services.impl.RateLimitingService;
 
 @Component
+@Slf4j
 public class RateLimitInterceptor implements HandlerInterceptor {
 
     private final RateLimitingService rateLimitingService;
@@ -27,12 +29,14 @@ public class RateLimitInterceptor implements HandlerInterceptor {
         String authorizationHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
         if (authorizationHeader == null || !authorizationHeader.startsWith("Bearer ")) {
             response.sendError(HttpStatus.UNAUTHORIZED.value(), "Missing or invalid Bearer token");
+            log.atError().log("Missing or invalid Bearer token");
             return false;
         }
 
         String apiKey = authorizationHeader.substring(7); // Extract the token excluding "Bearer "
         if (apiKey.isEmpty()) {
             response.sendError(HttpStatus.UNAUTHORIZED.value(), "Missing or invalid Bearer token");
+            log.atError().log("Missing or invalid Bearer token");
             return false;
         }
 
@@ -40,13 +44,14 @@ public class RateLimitInterceptor implements HandlerInterceptor {
         ConsumptionProbe probe = tokenBucket.tryConsumeAndReturnRemaining(1);
         if (probe.isConsumed()) {
             response.addHeader("X-Rate-Limit-Remaining", String.valueOf(probe.getRemainingTokens()));
+            log.atInfo().log("Remaining tokens: {}", probe.getRemainingTokens());
             return true;
         } else {
             long waitForRefill = probe.getNanosToWaitForRefill() / 1_000_000_000;
             response.setStatus(HttpStatus.TOO_MANY_REQUESTS.value());
             response.addHeader("X-Rate-Limit-Retry-After-Seconds", String.valueOf(waitForRefill));
-            response.sendError(HttpStatus.TOO_MANY_REQUESTS.value(),
-                    "You have exhausted your API Request Quota");
+            response.sendError(HttpStatus.TOO_MANY_REQUESTS.value(), "You have exhausted your API Request Quota");
+            log.atError().log("You have exhausted your API Request Quota");
             return false;
         }
     }
--- cli/utils/post_heartbeats.sh
+++ cli/utils/post_heartbeats.sh
@@ -5,12 +5,12 @@ api_url="http://localhost:8080/api/patients/1/heartbeats"
 bearer_token="eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxMjM0NTY3ODkiLCJpYXQiOjE3MDUyMjMyNTYsImV4cCI6MTcwNTIyNjg1Nn0.M63kEnIS1NhOMBLbuMBEQ2ud1a8IRVutQ347FfVa_Rk"
 
 # Loop for 20 POST requests with random heartbeat values ranging from 60 to 140
-for ((i = 0; i < 5; i++)); do
+for ((i = 0; i < 20; i++)); do
     random_heartbeat=$((RANDOM % (140 - 60 + 1) + 60))
     json_body="{\"heartbeat\":$random_heartbeat}"
     
     # Make POST request using curl
-    curl -i -X POST -H "Authorization: Bearer $bearer_token" -H "Content-Type: application/json" -d "$json_body" "$api_url"
+    curl -X POST -H "Authorization: Bearer $bearer_token" -H "Content-Type: application/json" -d "$json_body" "$api_url"
 
     echo ""
 done
